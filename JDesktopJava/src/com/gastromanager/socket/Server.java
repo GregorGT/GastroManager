0a1ceeef12021862d78fa983da689bc800ee4b97
Kalyan Mekala <kalyan.mekala@tui.be>
2021-05-09 02:40:07 +0530

    change: added threads to server socket program

--- JDesktopJava/src/com/gastromanager/socket/Server.java
+++ JDesktopJava/src/com/gastromanager/socket/Server.java
@@ -1,11 +1,16 @@
 package com.gastromanager.socket;
 
 
-import com.gastromanager.util.DbUtil;
+import com.gastromanager.print.PrintServiceImpl;
+import com.gastromanager.util.XmlUtil;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Server {
 
@@ -18,23 +23,20 @@ public class Server {
         try {
             //Connection to client
             serverSocket = new ServerSocket(serverSocketPort);
-            socket = serverSocket.accept();
+
 
             while(true) {
-                //Read data from client
-                in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
-                String orderId = in.readUTF();
-                System.out.println("Received request for "+ orderId);
-
-                while(null != orderId) {
-                    //send result to client
+                socket = null;
+                try{
+                    socket = serverSocket.accept();
+                    in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
                     out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
-                    out.writeUTF(DbUtil.loadOrderItemXmlInfo(orderId).toString());
-                    out.close();
+                    Thread t = new ClientHandler(socket, in, out);
+                    t.start();
+                } catch (Exception e) {
+                    socket.close();
+                    e.printStackTrace();
                 }
-                //socket.close();
-                in.close();
-                //serverSocket.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -42,7 +44,86 @@ public class Server {
 
     }
 
+    public void sendXMLData(DataOutputStream out) {
+        try {
+            String xmlContent = XmlUtil.readFileToString(
+                    "C:\\Users\\Admin\\IdeaProjects\\GastroManager\\DesktopJava\\data\\sample_tempalte.xml", Charset.defaultCharset());
+            System.out.println(xmlContent);
+            out.writeUTF(xmlContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
         Server server = new Server();
+    }
+    // ClientHandler class
+    class ClientHandler extends Thread {
+        final DataInputStream dis;
+        final DataOutputStream dos;
+        final Socket s;
+
+        public ClientHandler(Socket s, DataInputStream dis, DataOutputStream dos) {
+            this.s = s;
+            this.dis = dis;
+            this.dos = dos;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    //String orderId = in.readUTF();
+                    String request = in.readUTF().trim();
+                    System.out.println("Received request for " + request);
+
+                    if (isNumeric(request)) {
+                        while (null != request) {
+                            //send result to client
+                            //out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+                            //out.writeUTF(DbUtil.loadOrderItemXmlInfo(orderId).toString());
+                            out.writeUTF(new PrintServiceImpl().getPrintInfo(request));
+                            out.close();
+                        }
+                    } else {
+                        if (request.equals("menu")) {
+                            sendXMLData(out);
+                            out.close();
+                        }
+                    }
+
+                    if (request.equals("Exit")) {
+                        System.out.println("Client " + this.s + " sends exit...");
+                        System.out.println("Closing this connection.");
+                        this.s.close();
+                        System.out.println("Connection closed");
+                        break;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            try {
+                // closing resources
+                this.dis.close();
+                this.dos.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
