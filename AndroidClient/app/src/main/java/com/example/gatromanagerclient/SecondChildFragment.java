e03a4e6ffa85ceb54e2d462c426e17a6639a8920
Kalyan Mekala <kalyan.mekala@tui.be>
2021-05-10 01:36:25 +0530

    change:removed unused code

--- AndroidClient/app/src/main/java/com/example/gatromanagerclient/SecondChildFragment.java
+++ AndroidClient/app/src/main/java/com/example/gatromanagerclient/SecondChildFragment.java
@@ -1,5 +1,6 @@
 package com.example.gatromanagerclient;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -10,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
@@ -39,53 +41,58 @@ public class SecondChildFragment extends Fragment {
     public void createOrderDrillDown(String response, ViewGroup container) {
         System.out.println("inside creation of drill down "+response);
         LinearLayout linearLayout = (LinearLayout) container.findViewById(R.id.orderDrillDownLayout);
-
-        try {
-            Document menuXml = XmlUtil.loadXMLFromString(response);
-            NodeList drillDownMenusList = menuXml.getElementsByTagName("drilldownmenus"); //TODO fix this as its not returning all elements
-            NodeList drillDownMenuList = drillDownMenusList.item(0).getChildNodes();//menuXml.getElementById("drilldownmenus");//getElementsByTagName("drilldownmenu"); //TODO fix this as its not returning all elements
-            System.out.println("drill down menu types count "+drillDownMenuList.getLength());
-            for(int i=0; i < drillDownMenuList.getLength(); i++) {
-                Node node = drillDownMenuList.item(i);
-                if (node.hasAttributes()) {
-                    System.out.println("has attributes");
-                    Node nameAttributeNode =node.getAttributes().getNamedItem("name");
-                    System.out.println("NodeValue = "+nameAttributeNode.getNodeValue());
-                    if(null != nameAttributeNode.getNodeValue() &&
-                            nameAttributeNode.getNodeValue().equals("Android Tablet")) {
-                        System.out.println("Tablets");
-                        NodeList childNodes = node.getChildNodes();
-                        for(int j=0; j< childNodes.getLength(); j++) {
-                            Node childNode = childNodes.item(j);
-                            if(childNode.getNodeName().equals("button")) {
-                                String buttonText = childNode.getAttributes().getNamedItem("name").getNodeValue();
-                                String buttonHeight = null;
-                                String buttonWidth = null;
-                                NodeList childNodeChildren = childNode.getChildNodes();
-                                for(int k=0; k<childNodeChildren.getLength(); k++) {
-                                    Node node1 = childNodeChildren.item(k);
-                                    if(node1.getNodeName().equals("height")) {
-                                        buttonHeight = node1.getTextContent();
-                                    } else if (node1.getNodeName().equals("width")) {
-                                        buttonWidth = node1.getTextContent();
+        if(null == response) {
+            TextView msgView = new TextView(container.getContext());
+            msgView.setText("No data found");
+            linearLayout.addView(msgView);
+        } else {
+            try {
+                Document menuXml = XmlUtil.loadXMLFromString(response);
+                NodeList drillDownMenusList = menuXml.getElementsByTagName("drilldownmenus"); //TODO fix this as its not returning all elements
+                NodeList drillDownMenuList = drillDownMenusList.item(0).getChildNodes();//menuXml.getElementById("drilldownmenus");//getElementsByTagName("drilldownmenu"); //TODO fix this as its not returning all elements
+                System.out.println("drill down menu types count " + drillDownMenuList.getLength());
+                for (int i = 0; i < drillDownMenuList.getLength(); i++) {
+                    Node node = drillDownMenuList.item(i);
+                    if (node.hasAttributes()) {
+                        System.out.println("has attributes");
+                        Node nameAttributeNode = node.getAttributes().getNamedItem("name");
+                        System.out.println("NodeValue = " + nameAttributeNode.getNodeValue());
+                        if (null != nameAttributeNode.getNodeValue() &&
+                                nameAttributeNode.getNodeValue().equals("Android Tablet")) {
+                            System.out.println("Tablets");
+                            NodeList childNodes = node.getChildNodes();
+                            for (int j = 0; j < childNodes.getLength(); j++) {
+                                Node childNode = childNodes.item(j);
+                                if (childNode.getNodeName().equals("button")) {
+                                    String buttonText = childNode.getAttributes().getNamedItem("name").getNodeValue();
+                                    String buttonHeight = null;
+                                    String buttonWidth = null;
+                                    NodeList childNodeChildren = childNode.getChildNodes();
+                                    for (int k = 0; k < childNodeChildren.getLength(); k++) {
+                                        Node node1 = childNodeChildren.item(k);
+                                        if (node1.getNodeName().equals("height")) {
+                                            buttonHeight = node1.getTextContent();
+                                        } else if (node1.getNodeName().equals("width")) {
+                                            buttonWidth = node1.getTextContent();
+                                        }
                                     }
-                                }
 
-                                System.out.println("adding button for "+buttonText+ " "+buttonHeight+" "+buttonWidth);
-                                Button myNewButton = new Button(container.getContext());
-                                myNewButton.setText(buttonText);
-                                myNewButton.setHeight(Integer.parseInt(buttonHeight));
-                                myNewButton.setWidth(Integer.parseInt(buttonWidth));
-                                myNewButton.setVisibility(View.VISIBLE);
-                                linearLayout.addView(myNewButton);
+                                    System.out.println("adding button for " + buttonText + " " + buttonHeight + " " + buttonWidth);
+                                    Button myNewButton = new Button(container.getContext());
+                                    myNewButton.setText(buttonText);
+                                    myNewButton.setHeight(Integer.parseInt(buttonHeight));
+                                    myNewButton.setWidth(Integer.parseInt(buttonWidth));
+                                    myNewButton.setVisibility(View.VISIBLE);
+                                    linearLayout.addView(myNewButton);
+                                }
                             }
                         }
                     }
                 }
-            }
 
-        } catch (Exception e) {
-            e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
     }
@@ -112,15 +119,16 @@ public class SecondChildFragment extends Fragment {
         void messageFromChildFragment(Uri uri);
     }
 
-    private class LoadDrillDownMenuTask extends AsyncTask<Object, Void, String> {
+    private static class LoadDrillDownMenuTask extends AsyncTask<Object, Void, String> {
         SecondChildFragment secondChildFragment;
         String requestIdentifier;
+        @SuppressLint("StaticFieldLeak")
         ViewGroup container;
         @RequiresApi(api = Build.VERSION_CODES.N)
         @Override
         protected String doInBackground(Object... request) {
             Iterator iterator = Arrays.stream(request).iterator();
-            Integer paramCount = 0;
+            int paramCount = 0;
             while(iterator.hasNext()) {
                 Object param = iterator.next();
                 switch (paramCount) {
@@ -133,9 +141,9 @@ public class SecondChildFragment extends Fragment {
                 paramCount++;
             }
             Client client = new Client();//Client.getInstance();
-            String serverResponse = client.getResponse((String) Arrays.stream(request).findFirst().get());
-            System.out.println("Requesting for "+request);
-            //System.out.println("Received "+serverResponse);
+            String serverResponse = client.getResponse(requestIdentifier);
+            System.out.println("Requesting for "+requestIdentifier);
+            System.out.println("Received "+serverResponse);
             return serverResponse;
         }
         @Override
