585bf19fce29015b8fce35836d3004ba6893ff58
Kalyan Mekala <kalyan.mekala@tui.be>
2021-06-08 21:36:45 +0530

    Change: changes to insert a new order item

---
+++ JDesktopJava/src/com/gastromanager/models/xml/Option.java
@@ -0,0 +1,66 @@
+package com.gastromanager.models.xml;
+
+import javax.xml.bind.annotation.*;
+
+@XmlRootElement(name="option")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Option
+{
+    @XmlAttribute
+    private String price;
+
+    @XmlAttribute
+    private String name;
+
+    @XmlAttribute
+    private String id;
+
+    @XmlElement
+    private Choice choice;
+
+    public String getPrice ()
+    {
+        return price;
+    }
+
+    public void setPrice (String price)
+    {
+        this.price = price;
+    }
+
+    public String getName ()
+    {
+        return name;
+    }
+
+    public void setName (String name)
+    {
+        this.name = name;
+    }
+
+    public String getId ()
+    {
+        return id;
+    }
+
+    public void setId (String id)
+    {
+        this.id = id;
+    }
+
+    public Choice getChoice ()
+    {
+        return choice;
+    }
+
+    public void setChoice (Choice choice)
+    {
+        this.choice = choice;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [price = "+price+", name = "+name+", id = "+id+", choice = "+choice+"]";
+    }
+}
