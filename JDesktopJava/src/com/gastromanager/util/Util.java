cf9d3211174d86db9fe09506be6f659143f3aa46
GregorGT <schilling.gregor@gmail.com>
2021-11-03 09:06:15 +0100

    adding double rounding function for taxations

--- JDesktopJava/src/com/gastromanager/util/Util.java
+++ JDesktopJava/src/com/gastromanager/util/Util.java
@@ -1,5 +1,8 @@
 package com.gastromanager.util;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Util {
     public static boolean isNumeric(String str) {
         try {
@@ -8,5 +11,12 @@ public class Util {
         } catch (NumberFormatException e) {
             return false;
         }
+    }
+    
+    public static double roundDouble(double d, int places) {
+    	 
+        BigDecimal bigDecimal = new BigDecimal(Double.toString(d));
+        bigDecimal = bigDecimal.setScale(places, RoundingMode.HALF_UP);
+        return bigDecimal.doubleValue();
     }
 }
