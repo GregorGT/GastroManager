1591d21aedab909a271f7e5bddc36c834513a15f
PanagiotisAlimisis <panagiotisalimisis@gmail.com>
2021-11-08 20:50:19 +0200

    ReservationsExceedLimit, PaymentMenu GUI bug fixed, Version_TAG, MIT_Licence

--- JDesktopJava/src/com/gastromanager/mainwindow/MainWindow.java
+++ JDesktopJava/src/com/gastromanager/mainwindow/MainWindow.java
@@ -1,3 +1,13 @@
+/*Copyright 2021 GastroRice
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+
 package com.gastromanager.mainwindow;
 
 
@@ -46,6 +56,8 @@ import com.sun.star.xforms.Model;
 
 public class MainWindow extends JFrame {
 
+	private final static String VERSION_TAG = "v 1.0 11/8/2021";
+	
 	private JPanel contentPane;
 	private GMTreeItem root = new GMTreeItem("Restaurant name");
 	private GMTree tree;
@@ -161,12 +173,13 @@ public class MainWindow extends JFrame {
 
 		BookingsMenu tabBookings = new BookingsMenu();
 		tabbedPane.addTab("Bookings", null,tabBookings, null);
+		
 
-		ServerSocketMenu serverSocketMenu = new ServerSocketMenu();
+		ServerSocketMenu serverSocketMenu = new ServerSocketMenu(VERSION_TAG);
 		
 		
 		tabbedPane.addTab("Server/Settings", null, serverSocketMenu, null);
-
+		
 		JMenuItem mntmLoad = new JMenuItem("Load");
 		mntmLoad.addActionListener(new ActionListener() {
 
@@ -265,6 +278,13 @@ public class MainWindow extends JFrame {
 //			}
 //		});
 //		menuBar.add(btnDebugLoadFile);
+		
+		tabbedPane.addChangeListener(l -> {
+			if (tabbedPane.getSelectedComponent() instanceof BookingsMenu) {
+				tabBookings.fillTable();
+			}
+		
+		});
 	}
 
 	private void autoLoadPreviousFile() {
--- JDesktopJava/src/com/gastromanager/mainwindow/PaymentMenu.java
+++ JDesktopJava/src/com/gastromanager/mainwindow/PaymentMenu.java
@@ -1,3 +1,13 @@
+/*Copyright 2021 GastroRice
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+
 package com.gastromanager.mainwindow;
 
 import com.gastromanager.comparator.OrderItemComparator;
@@ -296,53 +306,7 @@ public class PaymentMenu  extends Panel{
 				List<OrderItemInfo> itemList = paymentService.processTransactionInfo(orderItemTransactionInfo);
 				((OrderItemListTableModel) selectedOrderItemsListTable.getModel()).getOrderItemInfoList().removeAll(orderItemInfoList);
 				((OrderItemListTableModel) selectedOrderItemsListTable.getModel()).fireTableDataChanged();
-				
-				
-//				StringBuilder bill = new StringBuilder();
-//				
-//				writeFileToBill("resources/billinghead.txt", bill);
-//				
-//				PrintServiceImpl printService = new PrintServiceImpl();
-//				
-//				bill.append("\n");
-//				DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-//				Date date = new Date();
-//				
-//				bill.append("Time: " + dateFormat.format(date) + "\n");
-//				//fileWriter.append("Server(terminal): " + ServerSocketMenu.serverTextField.getText() + "\n");//printService.getPrintInfo(txtFieldOrderID.getText(), "Server name", GastroManagerConstants.PRINT_RECEIPT));
-//				bill.append("\n");
-//				bill.append("\n");
-//				
-//				bill.append(printService.getPrintInfo(txtFieldOrderID.getText(), "", GastroManagerConstants.PRINT_RECEIPT));
-//				bill.append("\n");
-//				
-//				Double totalPrice = db.getTotalPrice(txtFieldOrderID.getText());
-//				Double salsetax = Double.parseDouble(PropertiesUtil.getPropertyValue("salsetax"));
-//				Double finalPrice = totalPrice + (totalPrice * (salsetax / 100));
-//			
-//				bill.append("--------------------------------\n");
-//				bill.append("SubTotal: " + GastroManagerConstants.FOUR_SPACES + totalPrice +"\n");
-//				bill.append("Taxes: " + GastroManagerConstants.FOUR_SPACES + PropertiesUtil.getPropertyValue("salsetax") + "%\n");
-//				bill.append("Total: " + GastroManagerConstants.FOUR_SPACES + finalPrice + PropertiesUtil.getPropertyValue("currency") + "\n");
-//				bill.append("--------------------------------\n");
-//				
-//				writeFileToBill("resources/billingfooter.txt", bill);
-//								
-//				System.out.println("This is the final bill\n\n\n\n");
-//				System.out.println(bill);
-//				try {
-//					printService.executePrintOverNetwork(bill.toString(), PropertiesUtil.getPropertyValue("networkPrinter.ip.billing"), Integer.parseInt(PropertiesUtil.getPropertyValue("networkPrinter.port.billing")));
-//				} catch( Exception ex)
-//				{
-//					System.out.print(ex.toString());
-//				}
-//				MainPrintService mainPrintService = new MainPrintService();
-//				try {
-//					mainPrintService.printString(PropertiesUtil.getPropertyValue("printer.billing"), bill.toString());
-//				}catch(Exception ex)
-//				{
-//					System.out.print(ex.toString());
-//				}
+				loadOrderItems(false, false);
 			}
 		});
 		this.add(payedButton);
@@ -408,24 +372,6 @@ public class PaymentMenu  extends Panel{
 		taxesAmountField.setText(String.valueOf(taxes));
 		ltotalAmountField.setText(String.valueOf(finalPrice));
 		
-	}
-
-	private void writeFileToBill(String path, StringBuilder bill) {
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(path));
-		    StringBuilder sb = new StringBuilder();
-		    String line = br.readLine();
-
-		    while (line != null) {
-		        sb.append(line);
-		        sb.append(System.lineSeparator());
-		        line = br.readLine();
-		    }
-		    String everything = sb.toString();
-		    bill.append(everything);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 	
 	private void addItemToTable(OrderItemInfo orderItemInfo, JTable table) {
